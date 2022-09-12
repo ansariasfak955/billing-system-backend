e358fd6f3d66796f32b32688e24973354d8ec689
Nishan Singh <nishan.singh@codingcafe.website>
2022-09-12 18:35:16 +0530

    update

--- resources/views/pdf/template.blade.php
+++ resources/views/pdf/template.blade.php
@@ -330,7 +330,15 @@
                 <th style="color: orange; border-bottom: 1px solid gray;text-align: left">{{ strtoupper($template->section) }} INFO</th>
                 <tr><td style="padding: 0; margin: 0;"><b>{{$template->client_legal_name}}({{$template->client_name}})</b></td></tr>
                 <tr><td style="padding: 0; margin: 0;"><b>{{$template->client_tin}}</b></td></tr>
-                <tr><td style="padding: 0; margin: 0;"><b>{{$template->client_phone}}</b></td></tr>
+                <tr><td style="padding: 0; margin: 0;"><b>
+                @if(@$client_phone_show['show'] ==1)
+                    @if(@$client_phone_show['show'] ==1 && @$client_phone_show['value'])
+                        {{$client_phone_show['value']}}
+                    @elseif(@$client_phone_show['show'] ==1 && @!$client_phone_show['value'])
+                        {{ $company->client_phone}}
+                    @endif
+                @endif
+               </b></td></tr>
             </table>
         </div>
         <div style="clear: both;"></div>
