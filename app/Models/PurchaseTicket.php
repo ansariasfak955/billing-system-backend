c083e5c9e842406163913156b627e39c71bf330c
abhishek-codingcafe <abhishek.kumar@codingcafe.website>
2022-10-10 17:40:37 +0530

    purchase changes

--- app/Models/PurchaseTicket.php
+++ app/Models/PurchaseTicket.php
@@ -13,10 +13,43 @@ class PurchaseTicket extends Model
 
     protected static $globalTable = 'purchase_tickets' ;
 
+    public $appends = ['paid_by_name', 'employee_name','supplier_name'];
+
     public function getTable() {
         return self::$globalTable ;
     }
     public static function setGlobalTable($table) {
         self::$globalTable = $table;
+    }
+
+    public function getSupplierNameAttribute(){
+        
+        if(isset( $this->attributes['supplier_id'] )){
+            $table = $this->getTable();
+            $company = filter_var($table, FILTER_SANITIZE_NUMBER_INT);
+            return get_supplier_name($company, $this->attributes['supplier_id']);
+        }
+    }
+    public function getPaidByNameAttribute(){
+        
+        if(isset( $this->attributes['paid_by'] )){
+            $table = $this->getTable();
+            $createdby = filter_var($table, FILTER_SANITIZE_NUMBER_INT);
+            return get_user_name($createdby, $this->attributes['paid_by']);
+        }
+    }
+    public function getEmployeeNameAttribute(){
+        
+        if(isset( $this->attributes['employee'] )){
+            $table = $this->getTable();
+            $createdby = filter_var($table, FILTER_SANITIZE_NUMBER_INT);
+            return get_user_name($createdby, $this->attributes['employee']);
+        }
+    }
+    public function getDateAttribute(){
+
+        if( isset( $this->attributes['date'] ) ){
+            return date( 'Y-m-d', strtotime($this->attributes['date']) );
+        }
     }
 }
