6b03f58ea0a5aae66da5fc0bcee9e1db8b129078
Md Asfak <md.asfak@codingcafe.website>
2022-12-02 17:44:59 +0530

    BILLING-282 import API

--- app/Imports/ServiceImport.php
+++ app/Imports/ServiceImport.php
@@ -27,28 +27,33 @@ class ServiceImport implements ToModel, WithHeadingRow
         Service::setGlobalTable($table);
         $row['reference'] = $request->reference;
 
+        if(@$row['id']){
+            unset($row['id']);
+        }
+
+        if(!@$row['tax']){
+            unset($row['tax']);
+        }
+
+        if(!@$row['is_active']){
+            unset($row['is_active']);
+        }
+
+        if(!@$row['is_promotional']){
+            unset($row['is_promotional']);
+        }
+
+
         if($request->tax){
             $row['tax']= $request->tax;
-        }else{
-            if(isset($row['tax'])){
-                unset($row['tax']);
-            }
         }
 
         if($request->is_active){
             $row['is_active']= $request->is_active;
-        }else{
-            if(isset($row['is_active'])){
-                unset($row['is_active']);
-            }
         }
 
         if($request->is_promotional){
             $row['is_promotional']= $request->is_promotional;
-        }else{
-            if(isset($row['is_promotional'])){
-                unset($row['is_promotional']);
-            }
         }
 
         $row['reference_number'] = get_service_latest_ref_number($this->company_id, $request->reference, 1);
