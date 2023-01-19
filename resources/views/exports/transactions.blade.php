1c8bb54e28519ac55b21552f33d0ff21e506557a
md asfak <md.asfak@codingcafe.website>
2023-01-19 12:43:55 +0530

    BILLING-456 transition export dynamic data

--- resources/views/exports/transactions.blade.php
+++ resources/views/exports/transactions.blade.php
@@ -1,13 +1,23 @@
 <table>
     <thead>
         <tr>
-            <td>text</td>
+            <td>Concept</td>
+            <td>Date</td>
+            <td>Payment Option</td>
+            <td>Back Account</td>
+            <td>Amount</td>
+            <td>Paid by</td>
         </tr>
     </thead>
     <tbody>
         @foreach($invoiceDeposits as $invoiceDeposit)
         <tr>
-            <td>text row</td>
+            <td>{{ @$invoiceDeposit->concept }}</td>
+            <td>{{ @$invoiceDeposit->date}}</td>
+            <td>{{ @$invoiceDeposit->payment_option}}</td>
+            <td></td>
+            <td>{{ @$invoiceDeposit->amount}}</td>
+            <td>{{ @$invoiceDeposit->paid_by}}</td>
         </tr>
         @endforeach
     </tbody>
