ca8ce3ef322b396ace130ec47da82d67c9a0225f
md asfak <md.asfak@codingcafe.website>
2023-01-20 11:29:01 +0530

    BILLING-456 payment option name

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -21,12 +21,28 @@ class Deposit extends Model
     ];
 
     protected static $globalTable = 'deposits' ;
-
+    protected $appends = ['paid_by_name','payment_option_name'];
     public function getTable() {
         return self::$globalTable ;
     }
     public static function setGlobalTable($table) {
         self::$globalTable = $table;
+    }
+    public function getPaidByNameAttribute(){
+        
+        if(isset( $this->attributes['created_by'] )){
+            $table = $this->getTable();
+            $createdby = filter_var($table, FILTER_SANITIZE_NUMBER_INT);
+            return get_user_name($createdby, $this->attributes['created_by']);
+        }
+    }
+    public function getPaymentOptionNameAttribute(){
+        
+        if(isset( $this->attributes['payment_option'] )){
+            $table = $this->getTable();
+            $createdby = filter_var($table, FILTER_SANITIZE_NUMBER_INT);
+            return get_payment_option_name($createdby, $this->attributes['payment_option']);
+        }
     }
     public function modelFilter()
     {
