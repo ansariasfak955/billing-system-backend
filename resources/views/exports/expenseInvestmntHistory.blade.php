6fab3eef9382a780705936c0495c42ed8d4a5c0d
md asfak <md.asfak@codingcafe.website>
2023-02-02 18:41:03 +0530

    expense dynanmic data

--- resources/views/exports/expenseInvestmntHistory.blade.php
+++ resources/views/exports/expenseInvestmntHistory.blade.php
@@ -12,13 +12,10 @@
             <th>Supplier Phone 2</th>
             <th>Status</th>
             <th>Payment Option</th>
-            <th>Bank Account</th>
-            <th>BIC/SWIFT</th>
+            <th>Bank Account</th>>
             <th>Created By</th>
             <th>Agent</th>
-            <th>Primary Tax</th>
-            <th>Secondary Tax</th>
-            <th>Secondary Tax</th>
+            <th>Income Tax</th>
             <th>Total</th>
             <th>Total Tax</th>
             <th>Total Amount</th>
@@ -33,15 +30,42 @@
             <th>Private Comments</th>
             <th>Addendum</th>
             <th>Signed</th>
-            <th>Unit price</th>
             <th>Quantity</th>
-            <th>Product total</th>
         </tr>
     </thead>
     <tbody>
         @foreach($expenseHistorys as $expenseHistory)
         <tr>
-            <td>{{$expenseHistory->reference.''.$expenseHistory->reference_number}}</td>
+            <td>{{@$expenseHistory->reference.''.@$expenseHistory->reference_number}}</td>
+            <td>{{@$expenseHistory->date}}</td>
+            <td>{{@$expenseHistory->title}}</td>
+            <td>{{@$expenseHistory->supplier->legal_name}}</td>
+            <td>{{@$expenseHistory->supplier->tin}}</td>
+            <td>{{@$expenseHistory->category->name}}</td>
+            <td>{{@$expenseHistory->supplier->email}}</td>
+            <td>{{@$expenseHistory->supplier->phone_1}}</td>
+            <td>{{@$expenseHistory->supplier->phone_2}}</td>
+            <td>{{@$expenseHistory->status}}</td>
+            <td>{{@$expenseHistory->payment_options->name}}</td>
+            <td>{{@$expenseHistory->bank_account}}</td>
+            <td>{{@$expenseHistory->created_by_name}}</td>
+            <td>{{@$expenseHistory->agent_name}}</td>
+            <td>{{@$expenseHistory->amount_income_tax}}</td>
+            <td>{{@$expenseHistory->amount_with_out_vat}}</td>
+            <td>{{@$expenseHistory->amount - @$expenseHistory->amount_with_out_vat}}</td>
+            <td>{{@$expenseHistory->amount}}</td>
+            <td>{{@$expenseHistory->supplier->address}}</td>
+            <td>{{@$expenseHistory->supplier->city}}</td>
+            <td>{{@$expenseHistory->supplier->state}}</td>
+            <td>{{@$expenseHistory->supplier->zip_code}}</td>
+            <td>{{@$expenseHistory->supplier->country}}</td>
+            <td>{{@$expenseHistory->currency}}</td>
+            <td>{{@$expenseHistory->currency_rate}}</td>
+            <td>{{@$expenseHistory->comments}}</td>
+            <td>{{@$expenseHistory->private_comments}}</td>
+            <td>{{@$expenseHistory->addendum}}</td>
+            <td>{{@$expenseHistory->signature}}</td>
+            <td>{{@$expenseHistory->tin}}</td>
         </tr>
         @endforeach
     </tbody>
