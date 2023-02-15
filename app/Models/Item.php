ca78d040c1af39a6de05392de2f3110e9363eec3
abhishek-codingcafe <abhishek.kumar@codingcafe.website>
2023-02-15 19:55:09 +0530

    --amount column

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -34,11 +34,36 @@ class Item extends Model
     public static function setGlobalTable($table) {
         self::$globalTable = $table;
     }
-    public $appends = ['amount'];
+    public $appends = ['amount', 'taxAmount'];
+    public function getTaxAmountAttribute(){
+        if(isset($this->attributes['base_price'])){
+            $tax = 0;
+            if(isset($this->attributes['vat'])){
+                $tax = (int)$this->attributes['vat'];
+            }
+            if(isset($this->attributes['quantity'])){
+                $quantity = $this->attributes['quantity'];
+            }else{
+                $quantity = 1;
+            }
 
+            $basePrice = (float)$this->attributes['base_price'];
+            $discount = (float)$this->attributes['discount'];
+            if($basePrice){
+
+                $amount = ($basePrice - ($basePrice * $discount / 100)) * $quantity;
+                $taxAmount = 0;
+                if($tax){
+                    $taxAmount = ($tax / 100) * $amount;
+                }
+                return round($taxAmount, 2);
+            }
+            return 0;
+        }
+    }
     public function getAmountAttribute(){
         if(isset($this->attributes['base_price'])){
-
+            $tax = 0;
             if(isset($this->attributes['vat'])){
                 $tax = (int)$this->attributes['vat'];
             }
