7241636e8432ec97af5b79e9b70b9f98dcc284d1
md asfak <md.asfak@codingcafe.website>
2023-02-21 22:18:42 +0530

    invoice

--- app/Http/Controllers/Api/InvoiceReceiptController.php
+++ app/Http/Controllers/Api/InvoiceReceiptController.php
@@ -178,14 +178,20 @@ class InvoiceReceiptController extends Controller
         //     "status" => true,
         //     "invoice" => InvoiceReceipt::with('invoice','items')->where('id', $request->invoice_receipt)->first()
         // ]);
-        
+        $status = 'paid';
+        $invoice  = InvoiceTable::find($invoiceReceipt->invoice_id);
+        $amountToSub = 0;
+        if($invoice->amount_due == $request->amount){
+            $amountToSub = $request->amount;
+            $status = 'partially paid';
+        }
         $amount = $invoiceReceipt->amount;
-        $invoiceReceipt->amount = $amount-($request->amount ?? 0);
+        $invoiceReceipt->amount = $amount-$amountToSub;
         if($request->paid){
-            $invoice  = InvoiceTable::find($invoiceReceipt->invoice_id);
+            $invoiceReceipt->paid = '1';
             if($invoice){
 
-                $invoice->status =  'partially paid';
+                $invoice->status =  $status;
                 $invoice->save();
             }
         }
