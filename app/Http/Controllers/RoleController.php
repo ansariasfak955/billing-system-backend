f173ed6ef12197d434583cf464f6695f2e50a83c
abhishek-codingcafe <abhishek.kumar@codingcafe.website>
2023-03-06 18:49:35 +0530

    --update permission from backend

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -9,6 +9,8 @@ use App\Models\Company;
 use App\DataTables\RoleDataTable;
 use Illuminate\Http\Request;
 use Validator;
+use Illuminate\Support\Facades\Schema;
+
 
 class RoleController extends Controller
 {
@@ -112,6 +114,7 @@ class RoleController extends Controller
     {
         $role = Role::where('id', $role->id)->first();
         $selected_permissions = Role::findByName($role->name)->permissions->pluck('id')->toArray();
+        $selected_permissions_name = Permission::whereIn('id', $request->permissions)->pluck('name')->toArray();
         $revoke_permissions = array_diff($selected_permissions, $request->permissions);
         $revoke_arr = array_values($revoke_permissions);
         // Revoking permissions
@@ -129,29 +132,39 @@ class RoleController extends Controller
             $_role = Role::where('id', $role->id)->first();
             $_role->givePermissionTo($permission);
         }
-        // foreach(Company::pluck('id') as $company_id){
-            
-        //     $role = Role::where('id', $role->id)->first();
-        //     $selected_permissions = Role::findByName($role->name)->permissions->pluck('id')->toArray();
-        //     $revoke_permissions = array_diff($selected_permissions, $request->permissions);
-        //     $revoke_arr = array_values($revoke_permissions);
-        //     // Revoking permissions
-        //     foreach ($revoke_arr as $key => $rev_permission_id) {
-        //         $rev_permission = Permission::where('id', $rev_permission_id)->first();
-        //         $role->revokePermissionTo($rev_permission);
-        //     }
-            
-        //     $role->update($request->all());
-        //     $role->save();
-    
-        //     // Give permissions
-        //     foreach($request->permissions as $permission_index => $permission_id){
-        //         $permission = Permission::where('id', $permission_id)->first();
-        //         $_role = Role::where('id', $role->id)->first();
-        //         $_role->givePermissionTo($permission);
-        //     }
+        
+        //update the permission in the compmay as well
+        foreach(Company::pluck('id') as $company_id){
+            //check if company has all the tables
+            if (Schema::hasTable('company_'.$company_id.'_roles') && Schema::hasTable('company_'.$company_id.'_permissions') && Schema::hasTable('company_'.$company_id.'_role_has_permissions')) {
 
-        // }
+                $roleTable = 'company_'.$company_id.'_roles';
+                $rolePermissionTable = 'company_'.$company_id.'_role_has_permissions';
+                $companyPermissionTable = 'company_'.$company_id.'_permissions';
+
+                $companyRole = \DB::table($roleTable)->where('name', $request->name)->first();
+                //check if role exists in the company
+                if($companyRole){
+                    //delete the existing permissions
+                    \DB::table($rolePermissionTable)->where('role_id', $companyRole->id)->delete();
+                    //set the permissions
+                    if($request->permissions){
+                        foreach($selected_permissions_name as $permission){
+                            //check if permission exists (and because the permission id can be different from master table so it's better to find permission first according to company and then give permission)
+                            $compayPermission = \DB::table($companyPermissionTable)->where('name', $permission)->first();
+                            if($compayPermission){
+                                //finally insert the permisson
+                                \DB::table($rolePermissionTable)->insert([
+                                    'permission_id' => $compayPermission->id,
+                                    'role_id' => $companyRole->id,
+                                ]);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
 
         return redirect()->route('roles.index')->withSuccess('Role Updated Successfully!'); 
     }
