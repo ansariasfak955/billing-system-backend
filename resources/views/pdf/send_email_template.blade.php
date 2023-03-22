dcc3cb97175e3a3a042ba8ef684344bda39e59e9
md asfak <md.asfak@codingcafe.website>
2023-03-22 19:40:10 +0530

    template

--- resources/views/pdf/send_email_template.blade.php
+++ resources/views/pdf/send_email_template.blade.php
@@ -547,7 +547,7 @@
         
                     @if(@$client_supplier_name_show || @$client_supplier_legal_name_show)
                         @if($invoiceData->client->legal_name)
-                            <tr><td style="padding: 0; margin: 0;">Name/Legal Name: <b>{{@$client_supplier_legal_name .@$invoiceData->client->legal_name}}({{@$client_supplier_name .@$invoiceData->client->name}})</b></td></tr>
+                            <tr><td style="padding: 0; margin: 0;">Name/Legal Name: <b>{{@$client_supplier_legal_name}} {{@$invoiceData->client->legal_name}} ({{@$client_supplier_name}})  {{@$invoiceData->client->name}}</b></td></tr>
                         @endif
                     @endif 
 
