765979c0948b47bff653b4a95db64bfa73efa36d
abhishek-codingcafe <abhishek.kumar@codingcafe.website>
2023-04-07 17:35:09 +0530

    --api added

--- routes/api.php
+++ routes/api.php
@@ -187,13 +187,13 @@ Route::group(['namespace' => 'Api'], function() {
     Route::group(['middleware' => ['auth:api']], function(){
         Route::apiResource('companies', CompanyController::class);
         Route::post('update-profile', 'UserController@updateProfile');
-        Route::get('get-current-user', function(){
-            return response()->json([
-                'success' => true,
-                'data' => \Auth::user(),
-            ]);
-        });
         Route::group(["prefix" => "{company_id}/"], function(){
+            Route::get('get-current-user', function(){
+                return response()->json([
+                    'success' => true,
+                    'data' => \Auth::user(),
+                ]);
+            });
             Route::get('dashboard-counts', 'DashboardController@index');
             Route::apiResource('bank_accounts', BankAccountController::class);
             Route::apiResource('product_categories', ProductCategoryController::class);
