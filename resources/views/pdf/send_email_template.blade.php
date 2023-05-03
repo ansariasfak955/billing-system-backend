6cd8c9d9361bfeb4709365feafb08db5c7a59c64
md asfak <md.asfak@codingcafe.website>
2023-05-03 16:10:25 +0530

    template

--- resources/views/pdf/send_email_template.blade.php
+++ resources/views/pdf/send_email_template.blade.php
@@ -1037,7 +1037,9 @@
                 </tr>
             </table>
             @if(@$show_footer_count)
-                <div style="margin-left:15%;margin-top:20px;width: 70%; border-bottom: 1px solid black; text-align: right;">1/1</div>
+                <div style="margin-left:15%;margin-top:20px;width: 70%; border-bottom: 1px solid black; text-align: right;">
+                    <span style="margin-left:100px;">1/1</span>
+                </div>
             @endif
         </div>
         @endif
