f68667fed492048fe39985f4639681e91775a5e4
md asfak <md.asfak@codingcafe.website>
2023-05-08 10:43:22 +0530

    spanish

--- resources/views/pdf/template.blade.php
+++ resources/views/pdf/template.blade.php
@@ -883,12 +883,14 @@
             </div>
 
             <div style="position: fixed; left: 0; bottom: 0; width: 100%;">
+            @if(@$hide_signed_box_show == 0)
+                @if(@$show_signed_title)
+                    <p style="font-weight: bold;">{{@$signed_box_text ?? 'Signed:'}}</p>
+                @endif
+            @endif
                 <table style="border-collapse: collapse; vertical-align: top; width: 100%;">
                     <tr>
                     @if(@$hide_signed_box_show == 0)
-                        @if(@$show_signed_title)
-                            <p style="font-weight: bold;"> {{@$signed_box_text ?? 'Signed:'}}</p>
-                        @endif
                         <td style="margin: 0;">
                             <div style="border: 1px solid gray; padding: 100px 70px 10px 10px;">
                                 <!-- <img width="100" height="80" object-fit="cover"
@@ -927,12 +929,14 @@
         @else
         
         <div style="position: fixed; left: 0; bottom: 0; width: 100%;">
+            @if(@$hide_signed_box_show == 0)
+                @if(@$show_signed_title)
+                    <p style="font-weight: bold;">{{@$signed_box_text ?? 'Signed:'}}</p>
+                @endif
+            @endif
             <table style="border-collapse: collapse; vertical-align: top; width: 100%;">
                 <tr>
                     @if(@$hide_signed_box_show == 0)
-                        @if(@$show_signed_title)
-                            <p style="font-weight: bold;">{{@$signed_box_text ?? 'Signed:'}}</p><br>
-                        @endif
                             <td style="margin: 0;">
                             <div style="border: 1px solid gray; padding: 100px 70px 10px 10px;">
                                 <!-- <img width="100" height="80" object-fit="cover"
